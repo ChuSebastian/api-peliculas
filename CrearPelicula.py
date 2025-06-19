1efb9e57fe02e4bd055f6b260fc44660f8d912ad
Sebastian Chu <45053945+ChuSebastian@users.noreply.github.com>
2025-06-18 23:25:31 -0500

    commit

--- CrearPelicula.py
+++ CrearPelicula.py
@@ -1,27 +1,61 @@
 import boto3
 import uuid
 import os
+import json
 
 def lambda_handler(event, context):
-    # Entrada (json)
-    print(event) # Log json en CloudWatch
-    tenant_id = event['body']['tenant_id']
-    pelicula_datos = event['body']['pelicula_datos']
-    nombre_tabla = os.environ["TABLE_NAME"]
-    # Proceso
-    uuidv4 = str(uuid.uuid4())
-    pelicula = {
-        'tenant_id': tenant_id,
-        'uuid': uuidv4,
-        'pelicula_datos': pelicula_datos
-    }
-    dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table(nombre_tabla)
-    response = table.put_item(Item=pelicula)
-    # Salida (json)
-    print(pelicula) # Log json en CloudWatch
-    return {
-        'statusCode': 200,
-        'pelicula': pelicula,
-        'response': response
-    }
+    try:
+        # Entrada (json)
+        print(json.dumps({
+            "tipo": "INFO",
+            "log_datos": {
+                "mensaje": "Evento recibido",
+                "event": event
+            }
+        }))
+
+        tenant_id = event['body']['tenant_id']
+        pelicula_datos = event['body']['pelicula_datos']
+        nombre_tabla = os.environ["TABLE_NAME"]
+
+        # Proceso
+        uuidv4 = str(uuid.uuid4())
+        pelicula = {
+            'tenant_id': tenant_id,
+            'uuid': uuidv4,
+            'pelicula_datos': pelicula_datos
+        }
+
+        dynamodb = boto3.resource('dynamodb')
+        table = dynamodb.Table(nombre_tabla)
+        response = table.put_item(Item=pelicula)
+
+        # Salida (json)
+        print(json.dumps({
+            "tipo": "INFO",
+            "log_datos": {
+                "mensaje": "Película creada exitosamente",
+                "pelicula": pelicula,
+                "response": response
+            }
+        }))
+
+        return {
+            'statusCode': 200,
+            'pelicula': pelicula,
+            'response': response
+        }
+
+    except Exception as e:
+        print(json.dumps({
+            "tipo": "ERROR",
+            "log_datos": {
+                "mensaje": "Error al crear película",
+                "error": str(e)
+            }
+        }))
+
+        return {
+            'statusCode': 500,
+            'error': str(e)
+        }
